ce866e8f81c1abe365d28346c7bbc988d6c268d2
Alex Kustanovich <ak1970@gmail.com>
2018-07-12 17:29:11 -0400

    Add Standard and Ranked Books

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+  class RankedGradeBook : BaseGradeBook
+  {
+    public RankedGradeBook(string name) : base(name)
+    {
+      Type = Enums.GradeBookType.Ranked;
+    }
+  }
+}
