808582c58b71b5dc06d9e56fa3b5a2d89ed3b564
Alex Kustanovich <ak1970@gmail.com>
2018-07-13 09:43:55 -0400

    Add code to RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,14 +1,50 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook : BaseGradeBook
-    {
+  public class RankedGradeBook : BaseGradeBook
+  {
     public RankedGradeBook(string name) : base(name)
     {
       Type = Enums.GradeBookType.Ranked;
     }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+      var StudentsCount = Students.Count;
+      if (StudentsCount <= 5)
+      {
+        throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+      }
+
+      var stdpcnt = (int)Math.Ceiling(StudentsCount * 0.2);
+      var grades = Students.OrderByDescending(e => e.AverageGrade)
+                          .Select(e => e.AverageGrade)
+                          .ToList();
+
+      if (grades[stdpcnt - 1] <= averageGrade)
+      {
+        return 'A';
+      }
+      else if (grades[(stdpcnt * 2) - 1] <= averageGrade)
+      {
+        return 'B';
+      }
+      else if (grades[(stdpcnt * 3) - 1] <= averageGrade)
+      {
+        return 'C';
+      }
+      else if (grades[(stdpcnt * 4) - 1] <= averageGrade)
+      {
+        return 'D';
+      }
+      else
+      {
+        return 'F';
+      }
     }
+  }
 }
